1a53506df1a7b3d1be28c9dfdb0afaba4686a820
Anton Nikiforov <anton.ni@kodolabs.com>
2022-12-09 18:04:20 +0300

    Improve rendering

--- main.rb
+++ main.rb
@@ -6,20 +6,25 @@ Bundler.require
 
 WIDTH = 800
 HEIGHT = 600
-
+POINTS_PER_FRAME = 100
 serpinsky_fractal = Serpinsky.new(width: WIDTH, height: HEIGHT, points_count: 10_000)
 
 # Window rendering
 set width: WIDTH, height: HEIGHT
-
 points_count = 0
 text = Text.new("Points count: #{points_count}")
 
 update do
-  point = serpinsky_fractal.spawn_point
-  c = Circle.new(x: point.x, y: HEIGHT - point.y, radius: 1, color: 'white')
-  points_count += 1
+  POINTS_PER_FRAME.times do
+    serpinsky_fractal.spawn_point
+    points_count += 1
+  end
+
+  points_count += POINTS_PER_FRAME
   text.text = "Points count: #{points_count}"
+  serpinsky_fractal.points.each do |point|
+    Pixel.draw(x: point.x, y: HEIGHT - point.y, size: 1, color: [255, 255, 255, 1.0])
+  end
 end
 
 show
--- serpinsky.rb
+++ serpinsky.rb
@@ -1,7 +1,7 @@
 require_relative 'geometry/point'
 
 class Serpinsky
-  attr_reader :points_count, :width, :height, :initial_points, :points
+  attr_reader :points_count, :width, :height, :initial_points, :last_point, :points
 
   def initialize(points_count:, width:, height:)
     @points_count = points_count
@@ -13,20 +13,18 @@ class Serpinsky
       Point.new(width, 0),
       Point.new(width / 2, height)
     ]
-    @points = [Point.new(width / 2, height / 2)]
+    @last_point = Point.new(width / 2, height / 2)
   end
 
   def spawn_point
     point_a = initial_points.sample
-    point_b = points.sample
+    point_b = last_point
 
-    new_point = Point.new(
+    @last_point = Point.new(
       (point_a.x + point_b.x) / 2,
       (point_a.y + point_b.y) / 2
     )
 
-    points << new_point
-
-    new_point
+    points << last_point
   end
 end
