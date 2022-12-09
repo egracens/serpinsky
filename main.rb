14b97aa6b328c95dc544a1cf48ced2b537ce7c55
Anton Nikiforov <anton.ni@kodolabs.com>
2022-12-09 13:35:24 +0300

    Cleanup code

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 require 'bundler/setup'
-require_relative 'serpinsky'
 require 'ruby2d'
+require_relative 'serpinsky'
 
 Bundler.require
 
@@ -10,8 +10,9 @@ HEIGHT = 600
 serpinsky_fractal = Serpinsky.new(width: WIDTH, height: HEIGHT, points_count: 10_000)
 serpinsky_fractal.compute
 
+# Window rendering
 set width: WIDTH, height: HEIGHT
-puts serpinsky_fractal.points
+
 serpinsky_fractal.points.each do |point|
   c = Circle.new(x: point.x, y: point.y, radius: 1, color: 'white')
 end
